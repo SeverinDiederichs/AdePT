c83f2e437c88d1eab4be3e36b99811e675f045c2
SeverinDiederichs <severin.diederichs@cern.ch>
2025-01-27 16:17:27 +0100

    revert changes that weren't meant to be pushed

--- examples/IntegrationBenchmark/ci_tests/validation_testem3.sh
+++ examples/IntegrationBenchmark/ci_tests/validation_testem3.sh
@@ -20,12 +20,12 @@ CI_TEST_DIR=${PROJECT_SOURCE_DIR}/examples/IntegrationBenchmark/ci_tests
 # Define cleanup function of temporary files
 cleanup() {
   echo "Cleaning up temporary files..."
-  rm -f ${CI_TEST_DIR}/adept_em3_1e5_e-_ASYNC.csv ${CI_TEST_DIR}/adept_em3_1e5_e-_global.csv \
+  rm -f ${CI_TEST_DIR}/adept_em3_1e5_e-.csv ${CI_TEST_DIR}/adept_em3_1e5_e-_global.csv \
         ${CI_TEST_DIR}/validation_testem3.mac
 }
 
 # register cleanup to be called on exit
-# trap cleanup EXIT
+trap cleanup EXIT
 # called it directly ensure clean environment
 cleanup
 
@@ -34,10 +34,10 @@ $CI_TEST_DIR/python_scripts/macro_generator.py \
     --template ${CI_TEST_DIR}/example_template.mac \
     --output ${CI_TEST_DIR}/validation_testem3.mac \
     --gdml_name ${PROJECT_SOURCE_DIR}/examples/data/testEm3.gdml \
-    --num_threads 8 \
-    --num_events 5000 \
-    --num_trackslots 1 \
-    --num_hitslots 24 \
+    --num_threads 4 \
+    --num_events 200 \
+    --num_trackslots 10 \
+    --num_hitslots 6 \
     --gun_type setDefault 
 
 
@@ -45,14 +45,14 @@ $CI_TEST_DIR/python_scripts/macro_generator.py \
 $ADEPT_EXECUTABLE --do_validation --allsensitive --accumulated_events \
                   -m "${CI_TEST_DIR}/validation_testem3.mac" \
                   --output_dir "${CI_TEST_DIR}" \
-                  --output_file "adept_em3_1e5_e-_ASYNC1"
+                  --output_file "adept_em3_1e5_e-"
 
 # Validating the relative error per layer
 # The saved G4 benchmark file was run with --noadept and 1e7 primary 10 GeV electrons.
 # Comparing against 1e5 primary electrons (500 per event, 200 events) with AdePT should give errors below 1%.
 # This is a compromise between run time and accuracy of the test 
-$CI_TEST_DIR/python_scripts/check_validation.py --file1 ${CI_TEST_DIR}/adept_em3_1e5_e-_ASYNC1.csv \
+$CI_TEST_DIR/python_scripts/check_validation.py --file1 ${CI_TEST_DIR}/adept_em3_1e5_e-.csv \
                                                 --file2 ${CI_TEST_DIR}/benchmark_files/g4_em3_10e7_e-.csv \
-                                                --n1 1e6 --n2 1e7 --tol 0.01 \
-                                                --plot_file plot_ASYNC.png # uncomment to plot the validation plot
+                                                --n1 1e5 --n2 1e7 --tol 0.01 \
+                                                # --plot_file plot.png # uncomment to plot the validation plot
 
